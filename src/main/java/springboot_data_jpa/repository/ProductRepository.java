6c64cca3fedbb1282c386e00a12e540365de5bd2
vikasghanwat <vikasghanwat777@gmail.com>
2026-06-29 19:49:32 +0530

    Module 3.2 - Implement JPA repository queries with test cases

--- src/main/java/springboot_data_jpa/repository/ProductRepository.java
+++ src/main/java/springboot_data_jpa/repository/ProductRepository.java
@@ -1,12 +1,35 @@
 package springboot_data_jpa.repository;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 import springboot_data_jpa.entities.ProductEntity;
+
+import java.math.BigDecimal;
+import java.time.LocalDateTime;
+import java.util.List;
+import java.util.Optional;
 
 @Repository
 public interface ProductRepository extends JpaRepository<ProductEntity, Long> {
 
-    public ProductEntity findByTitle(String title);
+    public List<ProductEntity> findByTitle(String title);
+
+    List<ProductEntity> findByCreatedAtAfter(LocalDateTime time);
+
+    List<ProductEntity> findByQuantityAndPrice(int quantity, BigDecimal price);
+
+    List<ProductEntity> findByQuantityGreaterThanAndPriceLessThan(int i, BigDecimal bigDecimal);
+
+    List<ProductEntity> findByQuantityGreaterThanOrPriceLessThan(int i, BigDecimal bigDecimal);
+
+    List<ProductEntity> findByTitleLike(String title);
+
+    List<ProductEntity> findByTitleContains(String title);
+
+    List<ProductEntity> findByTitleContainingIgnoreCase(String title);
+
+    @Query("select e from ProductEntity e where e.title=?1 and e.price=?2")
+    Optional<ProductEntity> findByTitleAndPrice(String title, BigDecimal price);
 
 }
--- src/test/java/springboot_data_jpa/SpringbootDataJpaApplicationTests.java
+++ src/test/java/springboot_data_jpa/SpringbootDataJpaApplicationTests.java
@@ -1,13 +1,67 @@
 package springboot_data_jpa;
 
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import springboot_data_jpa.entities.ProductEntity;
+import springboot_data_jpa.repository.ProductRepository;
+
+import java.math.BigDecimal;
+import java.util.List;
+import java.util.Optional;
 
 @SpringBootTest
 class SpringbootDataJpaApplicationTests {
 
-	@Test
-	void contextLoads() {
-	}
+    @Autowired
+    private ProductRepository productRepository;
 
+    @Test
+    void contextLoads() {
+    }
+
+/*    @Test
+    void testRepository() {
+        ProductEntity build = ProductEntity.builder()
+                .sku("nestlay123")
+                .title("nestlay chocolate")
+                .price(BigDecimal.valueOf(13.45))
+                .quantity(12)
+                .build();
+
+        ProductEntity save = productRepository.save(build);
+        System.out.println(save);
+
+    }*/
+
+    @Test
+    void getRepository() {
+        // List<ProductEntity> entities = productRepository.findAll();
+
+        // List<ProductEntity> entities = productRepository.findByTitle("nestlay chocolate");
+
+        // List<ProductEntity> entities = productRepository.findByCreatedAtAfter(LocalDateTime.of(2025, 1, 1, 0, 0, 0));
+
+        // List<ProductEntity> entities = productRepository.findByQuantityAndPrice(12,BigDecimal.valueOf(13.45));
+        //List<ProductEntity> entities = productRepository.findByQuantityGreaterThanAndPriceLessThan(4,BigDecimal.valueOf(33.45));
+        //List<ProductEntity> entities = productRepository.findByQuantityGreaterThanOrPriceLessThan(4, BigDecimal.valueOf(33.45));
+
+        //List<ProductEntity> entities = productRepository.findByTitleLike("%choco%");
+        // List<ProductEntity> entities = productRepository.findByTitleContains("choco");
+        List<ProductEntity> entities = productRepository.findByTitleContainingIgnoreCase("choCO");
+        System.out.println(entities);
+
+    }
+
+/*    @Test
+    void getSingleEntityFromRepository() {
+        Optional<ProductEntity> entity = productRepository.findByTitleAndPrice("Pepsi", BigDecimal.valueOf(14.4));
+        entity.ifPresent(System.out::println);
+    }*/
+
+    @Test
+    void getSingleEntityFromRepositoryByQuery() {
+        Optional<ProductEntity> entity = productRepository.findByTitleAndPrice("Pepsi", BigDecimal.valueOf(14.4));
+        entity.ifPresent(System.out::println);
+    }
 }
